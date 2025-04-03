e989bc1a795be1c0b3db39348c5a87cea3c61cfe
mkngkm <mikang1031@gmail.com>
2025-04-03 15:28:29 +0900

    7-6

--- 7/7-6.py
+++ 7/7-6.py
@@ -26,3 +26,28 @@ if __name__=="__main__":
     print(cnt)
     
     
+## 아스키 코드를 거의 안쓰는 경우
+def DFS(L, P):
+    global cnt
+    if L == n:
+        cnt += 1
+        for j in range(P):
+            print(chr(res[j] + 64), end="")
+        print()
+    else:
+        for i in range(1, 27): # A 부터 Z 까지 다 확인
+            if code[L]==i:
+                res[P]=i
+                DFS(L+1, P+1)
+            elif i>=10 and code[L]==i//10 and code[L+1]==i%10:
+                res[P]=i
+                DFS(L+2, P+1)
+        
+if __name__=="__main__":
+    code = list(map(int, input()))
+    n = len(code)
+    code.insert(n, -1) # 에러 방지(2개 연달아 판별할떄)
+    res = [0] * (n+3)
+    cnt = 0
+    DFS(0, 0)
+    print(cnt)
